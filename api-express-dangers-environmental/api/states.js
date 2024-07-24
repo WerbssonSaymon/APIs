46b32d03cb2bef520cf7f60f94687dda1af58322
Werbsson Saymon <64561641+WerbssonSaymon@users.noreply.github.com>
2024-07-24 18:59:08 -0300

    organizando codigo

--- api-express-dangers-environmental/api/states.js
+++ api-express-dangers-environmental/api/states.js
@@ -1,31 +1,220 @@
 const states = [
-    { nome: 'Acre', sigla: 'AC', regiao: 'Norte',populacao: 881935, area: 164123.040, capital: 'Rio Branco' },
-    { nome: 'Alagoas', sigla: 'AL', regiao: 'Nordeste', populacao: 3337357, area: 27778.506, capital: 'Maceió' },
-    { nome: 'Amapá', sigla: 'AP', regiao: 'Norte', populacao: 845731, area: 142828.521, capital: 'Macapá' },
-    { nome: 'Amazonas', sigla: 'AM', regiao: 'Norte', populacao: 4144597, area: 1570745.680, capital: 'Manaus' },
-    { nome: 'Bahia', sigla: 'BA', regiao: 'Nordeste', populacao: 14873064, area: 564733.177, capital: 'Salvador' },
-    { nome: 'Ceará', sigla: 'CE', regiao: 'Nordeste', populacao: 9132078, area: 148920.472, capital: 'Fortaleza' },
-    { nome: 'Distrito Federal', sigla: 'DF', regiao: 'Centro-Oeste', populacao: 3015268, area: 5779.999, capital: 'Brasília' },
-    { nome: 'Espírito Santo', sigla: 'ES', regiao: 'Sudeste', populacao: 4018650, area: 46095.583, capital: 'Vitória' },
-    { nome: 'Goiás', sigla: 'GO', regiao: 'Centro-Oeste', populacao: 7018354, area: 340086.698, capital: 'Goiânia' },
-    { nome: 'Maranhão', sigla: 'MA', regiao: 'Nordeste', populacao: 7075181, area: 331937.450, capital: 'São Luís' },
-    { nome: 'Mato Grosso', sigla: 'MT', regiao: 'Centro-Oeste', populacao: 3484466, area: 903357.908, capital: 'Cuiabá' },
-    { nome: 'Mato Grosso do Sul', sigla: 'MS', regiao: 'Centro-Oeste', populacao: 2778986, area: 357125.000, capital: 'Campo Grande' },
-    { nome: 'Minas Gerais', sigla: 'MG', regiao: 'Sudeste', populacao: 21168791, area: 586528.293, capital: 'Belo Horizonte' },
-    { nome: 'Pará', sigla: 'PA', regiao: 'Norte', populacao: 8602865, area: 1247954.666, capital: 'Belém' },
-    { nome: 'Paraíba', sigla: 'PB', regiao: 'Nordeste', populacao: 4018127, area: 56585.000, capital: 'João Pessoa' },
-    { nome: 'Paraná', sigla: 'PR', regiao: 'Sul', populacao: 11433957, area: 199307.922, capital: 'Curitiba' },
-    { nome: 'Pernambuco', sigla: 'PE', regiao: 'Nordeste', populacao: 9557071, area: 98311.616, capital: 'Recife' },
-    { nome: 'Piauí', sigla: 'PI', regiao: 'Nordeste', populacao: 3273227, area: 251577.738, capital: 'Teresina' },
-    { nome: 'Rio de Janeiro', sigla: 'RJ', regiao: 'Sudeste', populacao: 17264943, area: 43780.172, capital: 'Rio de Janeiro' },
-    { nome: 'Rio Grande do Norte', sigla: 'RN', regiao: 'Nordeste', populacao: 3506853, area: 52811.047, capital: 'Natal' },
-    { nome: 'Rio Grande do Sul', sigla: 'RS', regiao: 'Sul', populacao: 11377239, area: 281730.223, capital: 'Porto Alegre' },
-    { nome: 'Rondônia', sigla: 'RO', regiao: 'Norte', populacao: 1777225, area: 237590.547, capital: 'Porto Velho' },
-    { nome: 'Roraima', sigla: 'RR', regiao: 'Norte', populacao: 605761, area: 224300.506, capital: 'Boa Vista' },
-    { nome: 'Santa Catarina', sigla: 'SC', regiao: 'Sul', populacao: 7164788, area: 95730.684, capital: 'Florianópolis' },
-    { nome: 'São Paulo', sigla: 'SP', regiao: 'Sudeste', populacao: 45919049, area: 248222.362, capital: 'São Paulo' },
-    { nome: 'Sergipe', sigla: 'SE', regiao: 'Nordeste', populacao: 2298696, area: 21915.116, capital: 'Aracaju' },
-    { nome: 'Tocantins', sigla: 'TO', regiao: 'Norte', populacao: 1572866, area: 277720.520, capital: 'Palmas' }
+    {
+        nome: 'Acre',
+        sigla: 'AC',
+        regiao: 'Norte',
+        populacao: 881935,
+        area: 164123.040,
+        capital: 'Rio Branco'
+    },
+    {
+        nome: 'Alagoas',
+        sigla: 'AL',
+        regiao: 'Nordeste',
+        populacao: 3337357,
+        area: 27778.506,
+        capital: 'Maceió'
+    },
+    {
+        nome: 'Amapá',
+        sigla: 'AP',
+        regiao: 'Norte',
+        populacao: 845731,
+        area: 142828.521,
+        capital: 'Macapá'
+    },
+    {
+        nome: 'Amazonas',
+        sigla: 'AM',
+        regiao: 'Norte',
+        populacao: 4144597,
+        area: 1570745.680,
+        capital: 'Manaus'
+    },
+    {
+        nome: 'Bahia',
+        sigla: 'BA',
+        regiao: 'Nordeste',
+        populacao: 14873064,
+        area: 564733.177,
+        capital: 'Salvador'
+    },
+    {
+        nome: 'Ceará',
+        sigla: 'CE',
+        regiao: 'Nordeste',
+        populacao: 9132078,
+        area: 148920.472,
+        capital: 'Fortaleza'
+    },
+    {
+        nome: 'Distrito Federal',
+        sigla: 'DF',
+        regiao: 'Centro-Oeste',
+        populacao: 3015268,
+        area: 5779.999,
+        capital: 'Brasília'
+    },
+    {
+        nome: 'Espírito Santo',
+        sigla: 'ES',
+        regiao: 'Sudeste',
+        populacao: 4018650,
+        area: 46095.583,
+        capital: 'Vitória'
+    },
+    {
+        nome: 'Goiás',
+        sigla: 'GO',
+        regiao: 'Centro-Oeste',
+        populacao: 7018354,
+        area: 340086.698,
+        capital: 'Goiânia'
+    },
+    {
+        nome: 'Maranhão',
+        sigla: 'MA',
+        regiao: 'Nordeste',
+        populacao: 7075181,
+        area: 331937.450,
+        capital: 'São Luís'
+    },
+    {
+        nome: 'Mato Grosso',
+        sigla: 'MT',
+        regiao: 'Centro-Oeste',
+        populacao: 3484466,
+        area: 903357.908,
+        capital: 'Cuiabá'
+    },
+    {
+        nome: 'Mato Grosso do Sul',
+        sigla: 'MS',
+        regiao: 'Centro-Oeste',
+        populacao: 2778986,
+        area: 357125.000,
+        capital: 'Campo Grande'
+    },
+    {
+        nome: 'Minas Gerais',
+        sigla: 'MG',
+        regiao: 'Sudeste',
+        populacao: 21168791,
+        area: 586528.293,
+        capital: 'Belo Horizonte'
+    },
+    {
+        nome: 'Pará',
+        sigla: 'PA',
+        regiao: 'Norte',
+        populacao: 8602865,
+        area: 1247954.666,
+        capital: 'Belém'
+    },
+    {
+        nome: 'Paraíba',
+        sigla: 'PB',
+        regiao: 'Nordeste',
+        populacao: 4018127,
+        area: 56585.000,
+        capital: 'João Pessoa'
+    },
+    {
+        nome: 'Paraná',
+        sigla: 'PR',
+        regiao: 'Sul',
+        populacao: 11433957,
+        area: 199307.922,
+        capital: 'Curitiba'
+    },
+    {
+        nome: 'Pernambuco',
+        sigla: 'PE',
+        regiao: 'Nordeste',
+        populacao: 9557071,
+        area: 98311.616,
+        capital: 'Recife'
+    },
+    {
+        nome: 'Piauí',
+        sigla: 'PI',
+        regiao: 'Nordeste',
+        populacao: 3273227,
+        area: 251577.738,
+        capital: 'Teresina'
+    },
+    {
+        nome: 'Rio de Janeiro',
+        sigla: 'RJ',
+        regiao: 'Sudeste',
+        populacao: 17264943,
+        area: 43780.172,
+        capital: 'Rio de Janeiro'
+    },
+    {
+        nome: 'Rio Grande do Norte',
+        sigla: 'RN',
+        regiao: 'Nordeste',
+        populacao: 3506853,
+        area: 52811.047,
+        capital: 'Natal'
+    },
+    {
+        nome: 'Rio Grande do Sul',
+        sigla: 'RS',
+        regiao: 'Sul',
+        populacao: 11377239,
+        area: 281730.223,
+        capital: 'Porto Alegre'
+    },
+    {
+        nome: 'Rondônia',
+        sigla: 'RO',
+        regiao: 'Norte',
+        populacao: 1777225,
+        area: 237590.547,
+        capital: 'Porto Velho'
+    },
+    {
+        nome: 'Roraima',
+        sigla: 'RR',
+        regiao: 'Norte',
+        populacao: 605761,
+        area: 224300.506,
+        capital: 'Boa Vista'
+    },
+    {
+        nome: 'Santa Catarina',
+        sigla: 'SC',
+        regiao: 'Sul',
+        populacao: 7164788,
+        area: 95730.684,
+        capital: 'Florianópolis'
+    },
+    {
+        nome: 'São Paulo',
+        sigla: 'SP',
+        regiao: 'Sudeste',
+        populacao: 45919049,
+        area: 248222.362,
+        capital: 'São Paulo'
+    },
+    {
+        nome: 'Sergipe',
+        sigla: 'SE',
+        regiao: 'Nordeste',
+        populacao: 2298696,
+        area: 21915.116,
+        capital: 'Aracaju'
+    },
+    {
+        nome: 'Tocantins',
+        sigla: 'TO',
+        regiao: 'Norte',
+        populacao: 1572866,
+        area: 277720.520,
+        capital: 'Palmas'
+    }
 ];
 
 export default states;
